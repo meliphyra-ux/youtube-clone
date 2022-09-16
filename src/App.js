e77bc036f066b1f5f0929a8ce68a5784b25e0c0c
meliphyra-ux <xennerit@icloud.com>
2022-09-16 16:58:08 +0300

    Created structure of project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter,Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
 
-const App = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{backgroundColor: '#000'}}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />}/>
+        <Route path="/channel/:id" element={<ChannelDetail />}/>
+        <Route path="/search/:searchTerm" element={<SearchFeed />}/>
+      </Routes>
+    </Box>
+  </BrowserRouter>
+  )
+    
+  
+
 
 export default App;
