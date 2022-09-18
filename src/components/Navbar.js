4e05e6072019e0bb89147d463554a91ee476d2de
meliphyra-ux <xennerit@icloud.com>
2022-09-18 16:37:57 +0300

    Almost finished Navbar. Need to set onClick's

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { Stack } from '@mui/material' 
-import { Link } from 'react-router-dom'
+import { Box, Stack, Typography } from '@mui/material'
+import { categories } from '../assets/utilities/categories'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Box sx={{
+      position: "fixed",
+      top: "60px",
+      left: "0px",
+      height: "100vh",
+      backgroundColor: "hsl(0, 0%, 18.82%)",
+      padding: "20px 150px 10px 15px",
+      color: "#fff"
+    }}>
+      <Stack spacing={2} direction="column">
+          {categories.map(category =>(
+            <Box display="flex" alignItems="center" sx={{
+              margin: "15px 0 !important"
+            }}>
+              {category.icon}
+              <Typography variant="p" sx={{
+                fontSize: "20px",
+                paddingLeft: "15px"
+              }}>
+                {category.name}
+              </Typography>
+            </Box>
+          ))}
+      </Stack>
+    </Box>
   )
 }
 
