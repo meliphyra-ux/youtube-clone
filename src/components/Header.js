b8dd0ec2a4606d5c1e2001840488bfd39eb3965f
meliphyra-ux <xennerit@icloud.com>
2022-09-25 17:12:04 +0300

    Optimized search

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { AppBar, Button, Box, OutlinedInput } from "@mui/material";
 
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = ({ isNavbarOpen, setIsNavbarOpen }) => {
+  const [searchValue, setSearchValue] = useState("");
 
-  const [searchValue, setSearchValue] = useState("")
+  const navigate = useNavigate();
 
   return (
     <AppBar
@@ -33,15 +34,15 @@ const Header = ({ isNavbarOpen, setIsNavbarOpen }) => {
           />
         </Button>
         <Link to="/">
-        <h1 className="flex flex-center">
-          <YouTubeIcon
-            fontSize="large"
-            sx={{
-              color: "#ff0000",
-            }}
-          />
-          YouTube
-        </h1>
+          <h1 className="flex flex-center">
+            <YouTubeIcon
+              fontSize="large"
+              sx={{
+                color: "#ff0000",
+              }}
+            />
+            YouTube
+          </h1>
         </Link>
       </Box>
       <Box display="flex" alignItems="center">
@@ -53,9 +54,26 @@ const Header = ({ isNavbarOpen, setIsNavbarOpen }) => {
           }}
           placeholder="Enter a request..."
           value={searchValue}
-          onChange={e => setSearchValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              searchValue
+                ? navigate(`/search/${searchValue}`)
+                : console.log("Write query");
+              setSearchValue("");
+            }
+          }}
+          onChange={(e) => setSearchValue(e.target.value)}
         />
-        <Link to={`/search/${searchValue}`}><Button onClick={() => null}><SearchIcon/></Button></Link>
+        <Link
+          onClick={() => setSearchValue("")}
+          to={
+            searchValue ? `/search/${searchValue}` : console.log("Write query")
+          }
+        >
+          <Button>
+            <SearchIcon />
+          </Button>
+        </Link>
       </Box>
     </AppBar>
   );
